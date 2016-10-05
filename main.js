1d7e1cd53440f9ffd71b900ba6a4a4efc8cb5999
Paul Thaden <paul.thaden@oracle.com>
2016-10-05 17:38:43 -0400

    electron-packager mods and zipped OS-specific executables

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const {app, BrowserWindow} = require('electron')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-app.on('ready', () => {
+function createWindow () {
     // Create the browser window.
 	win = new BrowserWindow({
 		  width: 800, 
@@ -17,4 +17,34 @@ app.on('ready', () => {
 
 	// and load the index.html of the built JET app.
 	win.loadURL('file://' + __dirname + '/web/index.html')
+	
+	// Emitted when the window is closed.
+	win.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
 })
